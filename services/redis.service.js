72e53e8d44f238b112e7741bc712c17f9f40621b
alefreis <alef.reis@linkapi.com.br>
2019-03-28 23:02:44 -0300

    starting redis service

---
+++ services/redis.service.js
@@ -0,0 +1,18 @@
+const redis = require('redis');
+
+class RedisService {
+  connect() {
+
+  }
+
+  insertKey(key, value) {
+
+  }
+
+  getKey(key) {
+
+  }
+
+}
+
+module.exports = new RedisService();
